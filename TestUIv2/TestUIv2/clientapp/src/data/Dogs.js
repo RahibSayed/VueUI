8bdf4b84cf42f77c33b09a6970ad050c018d2f86
Rahib SAYED <rahib.sayed@axa-uk.co.uk>
2019-06-21 17:11:27 +0100

    Bootstrap-Vue working, need to restructure components and look at repo architecture

---
+++ TestUIv2/TestUIv2/clientapp/src/data/Dogs.js
@@ -0,0 +1,39 @@
+﻿export default [
+    {
+        name: 'Sheeba',
+        breed: 'collie',
+        gender: 'female',
+        age: 7,
+        colour: 'black',
+        weight: 34,
+    },
+    {
+        name: 'Sheeba',
+        breed: 'collie',
+        gender: 'female',
+        age: 7,
+        colour: 'black',
+        weight: 34,
+    }, {
+        name: 'Sheeba',
+        breed: 'collie',
+        gender: 'female',
+        age: 7,
+        colour: 'black',
+        weight: 34,
+    }, {
+        name: 'Sheeba',
+        breed: 'collie',
+        gender: 'female',
+        age: 7,
+        colour: 'black',
+        weight: 34,
+    }, {
+        name: 'Sheeba',
+        breed: 'collie',
+        gender: 'female',
+        age: 7,
+        colour: 'black',
+        weight: 34,
+    }
+]
--- TestUIv2/TestUIv2/clientapp/src/views/Cats.vue
+++ TestUIv2/TestUIv2/clientapp/src/views/Cats.vue
@@ -1,5 +1,23 @@
 ﻿<template>
     <div>
         <h1>Cats</h1>
+        <div>
+            <b-table striped hover :items="cats"></b-table>
+        </div>
+        <div class="text-center my-3">
+            <b-button v-b-popover.hover="'I am popover content!'" title="Popover Title">Hover Me</b-button>
+        </div>
     </div>
+    
 </template>
+
+<script>
+import cats from '@/data/Cats'
+  export default {
+    data() {
+      return {
+        cats
+      }
+    }
+  }
+</script>
